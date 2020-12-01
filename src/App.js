ef99602b4df51736ea603269ff447510557949f8
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-12-01 22:55:57 +0200

    ny

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 import Searchbar from './components/Searchbar/Searchbar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
@@ -40,35 +39,33 @@ export default class App extends Component {
 
     DataApi.fetchDataWithQuery(query, page, key)
 
-    .then((data) => {
-      if (data.length < 1) {
-        this.setState({ error: true });
-      } else {
-        this.setState((prevState) => ({
-          data: [...prevState.data, ...data],
-          page: prevState.page + 1,
-          error: false,
-        }));
-
-      }
-    })
+      .then(data => {
+        if (data.length < 1) {
+          this.setState({ error: true });
+        } else {
+          this.setState(prevState => ({
+            data: [...prevState.data, ...data],
+            page: prevState.page + 1,
+            error: false,
+          }));
+        }
+      })
       .catch(error => this.setState({ error }))
       .finally(() => this.setState({ loading: false }));
   };
   handleSearch = queryOn => {
     this.setState({
-      data:[]
-    })
+      data: [],
+    });
     this.setState({
       query: queryOn,
-
     });
   };
-  openModal=largeImageURL=>{
-       this.setState({
+  openModal = largeImageURL => {
+    this.setState({
       largeImageURL: largeImageURL,
     });
-    }
+  };
 
   modalClose = () => {
     this.setState(prevState => ({ largeImageURL: !prevState.largeImageURL }));
@@ -76,19 +73,23 @@ export default class App extends Component {
 
   render() {
     const { data, loading, error, largeImageURL } = this.state;
-       return (
+    return (
       <div>
         <Searchbar onSubmit={this.handleSearch} />
         {error && <Loaders text="ERRoR" />}
-        {loading ? <Loaders text="Loading..." /> : <ImageGallery openModal={this.openModal} data={data} />}
+        {loading ? (
+          <Loaders text="Loading..." />
+        ) : (
+          <ImageGallery openModal={this.openModal} data={data} />
+        )}
         {data.length > 0 && <Button handleClick={this.fetchData} />}
-        {largeImageURL &&
+        {largeImageURL && (
           <Modal
             onSubmit={this.openModal}
             largeImageURL={largeImageURL}
-            modalClose={this. modalClose}
+            modalClose={this.modalClose}
           />
-        }
+        )}
       </div>
     );
   }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import PropTypes from "prop-types"
+import React from 'react';
+// import PropTypes from 'prop-types';
 
-function Button ({handleClick}) {
-  return(
+function Button({ handleClick }) {
+  return (
     <>
-    <button type="button" onClick={handleClick} className={Button}>more</button>
+      <button type="button" onClick={handleClick} className="Button">
+        more
+      </button>
     </>
-  )
-  };
+  );
+}
 export default Button;
 
-Button.propTypes={
-  onClick: PropTypes.func.isRequired,
-}
+// Button.propTypes={
+//   onClick: PropTypes.func.isRequired,
+// }
